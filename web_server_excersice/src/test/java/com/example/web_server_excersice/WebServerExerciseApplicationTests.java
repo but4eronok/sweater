9736dbb46b321701c88b0ce654147cdbfa8bf978
but4eronok <kirill8256@rambler.ru>
2021-08-15 23:07:03 +0300

    add

---
+++ web_server_excersice/src/test/java/com/example/web_server_excersice/WebServerExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_server_excersice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServerExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
